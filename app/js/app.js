8bdaa0061539b51478ed1238aaca2cd6ffad3c6c
Brenton "B-Train" Fletcher <i@bloople.net>
2013-10-16 01:47:59 +1100

    Abandon swiping for now; use large previous and next buttons, like existing stories rails app

--- app/js/app.js
+++ app/js/app.js
@@ -5,6 +5,21 @@ $(function() {
 
   $("#wrapper").twoup("none");
 
+  $("#page-back").click(function() {
+    $.twoup.page(-1, true);
+  });
+  $("#page-next").click(function() {
+    $.twoup.page(1, true);
+  });
+/*
+  $("#wrapper > div > div").hammer().on("drag swipeleft swiperight", function(event) {
+    if(Hammer.utils.isVertical(event.gesture.direction)) return;
+    event.gesture.preventDefault();
+
+    if(event.type == 'swipeleft') $.twoup.page(1, true);
+    else if(event.type == 'swiperight') $.twoup.page(-1, true);
+  });*/
+
   $.getJSON("data.json").done(function(data) {
     if(data.length == 0) alert("No data.json, or data invalid.");
 
@@ -13,13 +28,5 @@ $(function() {
     window.router = new router();
     router.init();
     if(location.hash == "#" || location.hash == "") location.hash = "#index!1";
-
-    $("#wrapper").hammer().on("drag swipeleft swiperight", function(event) {
-      if(Hammer.utils.isVertical(event.gesture.direction)) return;
-      event.gesture.preventDefault();
-
-      if(event.type == 'swipeleft') $.twoup.page(1, true);
-      else if(event.type == 'swiperight') $.twoup.page(-1, true);
-    });
   });
 });
